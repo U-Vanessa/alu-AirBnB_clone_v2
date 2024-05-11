c0e54175e064f367e423bfaddc168fac9fedf318
gyhaan <g.yhaan@alustudent.com>
2024-05-11 11:18:04 +0200

    Updated BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,18 @@ import time
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key,value in kwargs:
+                if key != "__class__":
+                    if key=="created_at" or key=="updated_at":
+                        setattr(self,key,datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else: 
+                        setattr(self,key,value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
 
     def __str__(self):
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -35,23 +35,10 @@ class TestBaseModel(unittest.TestCase):
 
 
     def test_save_updates_updated_at(self):
-        # Create an instance of BaseModel
-        model = BaseModel()
-
-        # Get the initial updated_at timestamp
-        initial_updated_at = model.updated_at
-
-        # Call the save method
-        model.save()
-
-        # Get the updated updated_at timestamp
+        initial_updated_at = self.model.updated_at
+        self.model.save()
         updated_updated_at = model.updated_at
-
-        # Assert that updated_at has been updated
         self.assertNotEqual(initial_updated_at, updated_updated_at)
-
-        # Assert that the new updated_at is approximately now
-        # We use a small sleep to ensure that time has passed between save and checking
         time.sleep(0.001)
         now = datetime.now()
         self.assertAlmostEqual(updated_updated_at, now, delta=datetime.timedelta(seconds=1))
