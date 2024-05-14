fb623563f40b06ef31a642f53d417980e19689d3
gyhaan <g.yhaan@alustudent.com>
2024-05-14 18:13:22 +0200

    code refactor

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,40 +1,48 @@
-import json
+#!/usr/bin/python3
+"""
+Module for serializing and deserializing data
+"""
 
+import json
+import os.path
 
 class FileStorage:
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    # Path to the JSON file
+    __file_path = "file.json"
+    # Dictionary to store objects
+    __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects"""
         return self.__objects
-    
 
     def new(self, obj):
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
-    
     def save(self):
-        new_dict = {}
+        """Serializes __objects to the JSON file"""
+        # Convert objects to dictionary format
+        json_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        # Write dictionary to JSON file
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_dict, f)
 
-        for key, value in self.__objects.items():
-            new_dict[key] = value.to_dict()
-
-        try:
-            with open(self.__file_path, 'w') as file:
-                json.dump(new_dict, file, indent=2)
-        except FileNotFoundError:
-            pass
-
-    
     def reload(self):
-         try:
-            with open(self.__file_path, 'r') as file:
-                obj_dict = json.load(file)
-
-                for key, value in obj_dict.items():
-                    self.__objects[key] = eval(
-                        f"{value['__class__']}(**{value})")
-
-         except FileNotFoundError:
-            pass
+        """Deserializes the JSON file to __objects"""
+        if os.path.exists(self.__file_path):
+            # Read JSON file
+            with open(self.__file_path, 'r') as f:
+                # Load JSON data into dictionary
+                json_dict = json.load(f)
+                # Loop through dictionary to recreate objects
+                for key, obj_dict in json_dict.items():
+                    # Split key to get class name and object ID
+                    class_name, obj_id = key.split('.')
+                    # Get class reference using eval
+                    obj_class = eval(class_name)
+                    # Create instance of class with dictionary data
+                    obj_instance = obj_class(**obj_dict)
+                    # Store instance in __objects
+                    self.__objects[key] = obj_instance
