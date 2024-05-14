bc5fa01ed5969597a5395b51fe041a640f239fab
gyhaan <g.yhaan@alustudent.com>
2024-05-14 20:20:28 +0200

    code refactor

--- console.py
+++ console.py
@@ -5,16 +5,16 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """EOF command to exit the program"""
         print() 
         return True
 
-    def emptyline(self):
+    def emptyline(self, line):
         """Do nothing on empty input"""
         pass
 
