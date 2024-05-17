606596f4976a700fb93733a3b258321b81c2940d
U-Vanessa <144015111+U-Vanessa@users.noreply.github.com>
2024-05-17 14:39:54 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 """
-This module contains the City class which inherits from BaseModel.
+This module contains the City class (Blueprint for creating City objects).
 """
 
 from models.base_model import BaseModel
@@ -8,11 +8,16 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """
-    This class represents a city in the AirBnB clone project.
+    This is the city class
 
     Attributes:
-        state_id (str): The ID of the state to which the city belongs.
-        name (str): The name of the city.
+        state_id (str): The state id
+        name (str): The name of the city
     """
     state_id = ""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.name = City.name
+        # self.state_id = City.state_id
